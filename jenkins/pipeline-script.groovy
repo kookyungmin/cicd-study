1e013a23628bb85b8d1f9087060dbe9126f4053f
gguri4549 <rudals4549@naver.com>
2025-11-28 14:58:44 +0900

    feat: jenkins Dockerfile & script 추가

---
+++ jenkins/pipeline-script.groovy
@@ -0,0 +1,89 @@
+pipeline {
+    agent any
+    
+    environment {
+        APP_REPO_URL = "https://github.com/kookyungmin/cicd-study"
+        GITHUB_CREDENTIAL_ID = "github-access-token"
+        DOCKERHUB_CREDENTIALS = credentials("dockerhub-token")
+        DOCKERHUB_REPOSITORY = "rudals4549/cicd-study"
+        TARGET_HOST = "ec2-43-201-8-169.ap-northeast-2.compute.amazonaws.com"
+    }
+
+    stages {
+        stage("Checkout Source As Tagname") {
+            steps {
+                script {
+                    try {
+                        if("${env.TAG_NAME}" == "origin/main") {
+                            print("selected origin/main")
+                            throw new Exception("Tag selection is required")
+                        }
+                    } catch(err) {
+                        echo "Caught: ${err}"
+                        currentBuild.result = "FAILURE"
+                    }
+                }
+                
+                checkout([
+                    $class: 'GitSCM', 
+                    branches: [[name: "refs/tags/${params.TAG_NAME}"]],
+                    userRemoteConfigs: [[ url: "${env.APP_REPO_URL}", credentialsId: "${env.GITHUB_CREDENTIAL_ID}" ]],
+                    extensions: [
+                        [$class: 'CloneOption', noTags: false, shallow: false]
+                    ]
+                ])
+            }
+        }
+        
+        stage("Build") {
+            steps {
+                dir('boot-app') {
+                    sh "chmod +x gradlew"
+                    sh "./gradlew clean test bootJar"
+                }
+            }
+        }
+            
+        stage("Create new Docker Image") {
+            steps {
+                sh "docker build --no-cache --platform linux/amd64 -t $DOCKERHUB_REPOSITORY:${TAG_NAME} ."
+            }
+        }
+        
+        stage("Login Docker Hub") {
+            steps {
+                sh "echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin"
+            }
+        }
+        
+        stage("Upload Docker Image") {
+            steps {
+                sh "docker push $DOCKERHUB_REPOSITORY:${TAG_NAME}"
+            }
+        }
+        
+        stage("Prune Docker") {
+            steps {
+                sh "docker rmi $DOCKERHUB_REPOSITORY:${TAG_NAME}"
+                sh "docker system prune"
+            }
+        }
+        
+        stage("Deploy to EC2") {
+            steps {
+                sshagent (credentials: ['jenkins-controller']) {
+                    sh """
+                        ssh -o StrictHostKeyChecking=no ubuntu@${TARGET_HOST} '
+                            pwd
+                            docker compose down
+                            docker rmi ${DOCKERHUB_REPOSITORY}:${params.TAG_NAME}
+                            docker pull ${DOCKERHUB_REPOSITORY}:${params.TAG_NAME}
+                            docker compose up -d
+                        '
+                    """
+                }
+            }
+        }
+    }
+}
+
