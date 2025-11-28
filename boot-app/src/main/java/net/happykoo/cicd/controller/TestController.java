662472558899e71403ed6ccd598a0bcca50b3383
gguri4549 <rudals4549@naver.com>
2025-11-28 21:33:03 +0900

    feat: blue & green 무중단 배포 docker-compose 작성

--- boot-app/src/main/java/net/happykoo/cicd/controller/TestController.java
+++ boot-app/src/main/java/net/happykoo/cicd/controller/TestController.java
@@ -1,16 +1,20 @@
 package net.happykoo.cicd.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/test")
+@RequestMapping("/api/v1/health")
 public class TestController {
+
+  @Value("${spring.application.name}")
+  private String applicationName;
 
   @GetMapping
   public ResponseEntity<String> test() {
-    return ResponseEntity.ok("hello");
+    return ResponseEntity.ok(String.format("%s status: OK", applicationName));
   }
 }
